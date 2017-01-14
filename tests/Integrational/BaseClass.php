dfdcedfb245ccf93958a3fe8b21dac75b9eb458d
Andrii Osmak <aoworld@meta.ua>
2017-01-14 13:46:49 +0000

    feature/additional-tests

--- tests/Integrational/BaseClass.php
+++ tests/Integrational/BaseClass.php
@@ -28,14 +28,9 @@ abstract class BaseClass extends \PHPUnit_Framework_TestCase
         $container = new Container;
         $service   = $container->make('Aosmak\Laravel\Layer\Sdk\Services\LayerService');
         $service->setConfig([
-            // 'LAYER_SDK_APP_ID'           => getenv('LAYER_SDK_APP_ID'),
-            // 'LAYER_SDK_AUTH'             => getenv('LAYER_SDK_AUTH'),
-            // 'LAYER_SDK_BASE_URL'         => getenv('LAYER_SDK_BASE_URL'),
-            // 'LAYER_SDK_SHOW_HTTP_ERRORS' => false,
-
-            'LAYER_SDK_APP_ID'           => '91edc2ca-0dee-11e6-8b73-e616010026f7',
-            'LAYER_SDK_AUTH'             => 'hhjHBOWMDSA2jS7xfeIEN3fYn7686lncQANh5L06PWlZQqTW',
-            'LAYER_SDK_BASE_URL'         => 'https://api.layer.com/apps/',
+            'LAYER_SDK_APP_ID'           => getenv('LAYER_SDK_APP_ID'),
+            'LAYER_SDK_AUTH'             => getenv('LAYER_SDK_AUTH'),
+            'LAYER_SDK_BASE_URL'         => getenv('LAYER_SDK_BASE_URL'),
             'LAYER_SDK_SHOW_HTTP_ERRORS' => false,
         ]);
         $this->service = $service;
