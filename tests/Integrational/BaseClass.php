4616a1c8a844701f4ec6069d746e3353a72377ef
Andrii Osmak <aoworld@meta.ua>
2017-01-14 11:45:11 +0000

    feature/additional-tests

--- tests/Integrational/BaseClass.php
+++ tests/Integrational/BaseClass.php
@@ -24,9 +24,9 @@ abstract class BaseClass extends \PHPUnit_Framework_TestCase
         $container = new Container;
         $service   = $container->make('Aosmak\Laravel\Layer\Sdk\Services\LayerService');
         $service->setConfig([
-            'LAYER_SDK_APP_ID'           => env('LAYER_SDK_APP_ID'),
-            'LAYER_SDK_AUTH'             => env('LAYER_SDK_AUTH'),
-            'LAYER_SDK_BASE_URL'         => env('LAYER_SDK_BASE_URL'),
+            'LAYER_SDK_APP_ID'           => getenv('LAYER_SDK_APP_ID'),
+            'LAYER_SDK_AUTH'             => getenv('LAYER_SDK_AUTH'),
+            'LAYER_SDK_BASE_URL'         => getenv('LAYER_SDK_BASE_URL'),
             'LAYER_SDK_SHOW_HTTP_ERRORS' => false,
         ]);
         $this->service = $service;
