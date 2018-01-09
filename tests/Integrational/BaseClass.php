22a4216ebb13dc1929cc3578f9287d8c3c4afd64
Andrii Osmak <aoworld@meta.ua>
2018-01-10 11:46:53 +1300

    improvement/api-version-to-config

--- tests/Integrational/BaseClass.php
+++ tests/Integrational/BaseClass.php
@@ -38,7 +38,7 @@ abstract class BaseClass extends \PHPUnit_Framework_TestCase
                 'LAYER_SDK_APP_ID'           => getenv('LAYER_SDK_APP_ID'),
                 'LAYER_SDK_AUTH'             => getenv('LAYER_SDK_AUTH'),
                 'LAYER_SDK_BASE_URL'         => getenv('LAYER_SDK_BASE_URL'),
-                'LAYER_SDK_API_VERSION'      => getenv('LAYER_SDK_API_VERSION'),
+                'LAYER_SDK_API_VERSION'      => '1.1',
                 'LAYER_SDK_SHOW_HTTP_ERRORS' => false
             ];
         }
