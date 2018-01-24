583061e9fa54256ddb79becd71b8e7ddf9dcbebb
Andrii Osmak <aoworld@meta.ua>
2018-01-24 15:51:36 +1300

    improvement/code-refactoring: interface update

--- src/Services/LayerServiceInterface.php
+++ src/Services/LayerServiceInterface.php
@@ -2,6 +2,17 @@
 
 namespace Aosmak\Laravel\Layer\Sdk\Services;
 
+use Aosmak\Laravel\Layer\Sdk\Services\Subservices\BaseService;
+use Aosmak\Laravel\Layer\Sdk\Services\Subservices\AnnouncementService;
+use Aosmak\Laravel\Layer\Sdk\Services\Subservices\ConversationService;
+use Aosmak\Laravel\Layer\Sdk\Services\Subservices\UserService;
+use Aosmak\Laravel\Layer\Sdk\Services\Subservices\IdentityService;
+use Aosmak\Laravel\Layer\Sdk\Services\Subservices\UserDataService;
+use Aosmak\Laravel\Layer\Sdk\Services\Subservices\RichContentService;
+use Aosmak\Laravel\Layer\Sdk\Services\Subservices\MessageService;
+use Aosmak\Laravel\Layer\Sdk\Services\Subservices\NotificationService;
+use Aosmak\Laravel\Layer\Sdk\Services\Subservices\DataService;
+
 /**
  * Interface LayerServiceInterface.
  * @package namespace Aosmak\Laravel\Layer\Sdk\Services;
@@ -9,32 +20,65 @@ namespace Aosmak\Laravel\Layer\Sdk\Services;
 interface LayerServiceInterface
 {
     /**
-     * Get an announcement service
-     */
-    public function getAnnouncementService();
-
-    /**
      * Get a conversation service
+     *
+     * @return Aosmak\Laravel\Layer\Sdk\Services\Subservices\ConversationService
      */
-    public function getConversationService();
+    public function getConversationService(): ConversationService;
 
     /**
      * Get a message service
+     *
+     * @return Aosmak\Laravel\Layer\Sdk\Services\Subservices\MessageService
      */
-    public function getMessageService();
-
-    /**
-     * Get a notification service
-     */
-    public function getNotificationService();
+    public function getMessageService(): MessageService;
 
     /**
      * Get a user service
+     *
+     * @return Aosmak\Laravel\Layer\Sdk\Services\Subservices\UserService
      */
-    public function getUserService();
+    public function getUserService(): UserService;
+
+    /**
+     * Get an identity service
+     *
+     * @return Aosmak\Laravel\Layer\Sdk\Services\Subservices\IdentityService
+     */
+    public function getIdentityService(): IdentityService;
 
     /**
      * Get a user data service
+     *
+     * @return Aosmak\Laravel\Layer\Sdk\Services\Subservices\UserDataService
      */
-    public function getUserDataService();
+    public function getUserDataService(): UserDataService;
+
+    /**
+     * Get an announcement service
+     *
+     * @return Aosmak\Laravel\Layer\Sdk\Services\Subservices\AnnouncementService
+     */
+    public function getAnnouncementService(): AnnouncementService;
+
+    /**
+     * Get a notification service
+     *
+     * @return Aosmak\Laravel\Layer\Sdk\Services\Subservices\NotificationService
+     */
+    public function getNotificationService(): NotificationService;
+
+    /**
+     * Get a data service
+     *
+     * @return Aosmak\Laravel\Layer\Sdk\Services\Subservices\DataService
+     */
+    public function getDataService(): DataService;
+
+    /**
+     * Get a rich content service
+     *
+     * @return Aosmak\Laravel\Layer\Sdk\Services\Subservices\RichContentService
+     */
+    public function getRichContentService(): RichContentService;
 }
